ab0c31dc00a67e061738c6bd502776c6a069158e
achimcc <achim.schneider@posteo.de>
2026-02-02 17:07:22 +0100

    feat: add finder configuration module

---
+++ modules/system/finder.nix
@@ -0,0 +1,14 @@
+{ ... }:
+
+{
+  system.defaults.finder = {
+    AppleShowAllExtensions = true;
+    AppleShowAllFiles = true;
+    ShowPathbar = true;
+    ShowStatusBar = true;
+    FXEnableExtensionChangeWarning = false;
+    FXPreferredViewStyle = "Nlsv";  # List view
+    QuitMenuItem = true;  # Finder beenden erlauben
+    _FXShowPosixPathInTitle = true;
+  };
+}
